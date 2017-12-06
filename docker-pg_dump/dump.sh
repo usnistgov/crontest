5db9a3ef8673ecee6d3cbb6d2f03fa5a06d10663
stevebannon <sbannon@nist.gov>
2017-12-06 11:54:10 -0500

    adding a find to remove dumps older than 3 days.

--- docker-pg_dump/dump.sh
+++ docker-pg_dump/dump.sh
@@ -11,4 +11,6 @@ FILE="/dump/$PREFIX-$DATE.sql"
 pg_dump -h db -U "$PGUSER" -f "$FILE" -d "$PGDB"
 gzip "$FILE"
 
+find . -mtime +3 -name '*.gz' -execdir rm -- '{}' \;
+
 echo "Job finished: $(date)"
